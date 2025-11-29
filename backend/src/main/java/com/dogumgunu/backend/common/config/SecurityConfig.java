54c284a8068fa4388dac99c64ff8bf1d8c7682e5
Enes PC <akbltenes638@gmail.com>
2025-11-29 13:08:10 +0300

    web

--- backend/src/main/java/com/dogumgunu/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/dogumgunu/backend/common/config/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://nurcaninhediyesi.vercel.app", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://nurcanim.me", "https://www.nurcanim.me"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
